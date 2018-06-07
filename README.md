b92e08468f4ba90697a1a0f916751c17e35b6956
Davide <davide.iaiunese@gmail.com>
2018-06-07 22:35:57 +0200

    Implemented v2

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+
+## Requirements 2
+The application should display two cats. Each cat includes the cat's name a picture of the cat text showing the number of clicks. 
+
+The specifics of the layout do not matter, so style it however you'd like.
+
+The number of clicks should increment when each cat picture is clicked.
+
+
 ## License
 The contents of this repository are covered under the [MIT License](/LICENSE?raw=true)
+
--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,18 @@
 // increment it by 1
 // update clicktracker string value
 
-let clickTracker = document.getElementById('clickTracker');
+let catOne = document.getElementById('clickTrackerOne');
+let catTwo = document.getElementById('clickTrackerTwo');
 
-let n = parseInt(clickTracker.innerHTML);
+let num1 = parseInt(catOne.innerHTML);
+let num2 = parseInt(catTwo.innerHTML);
 
-function incrementByOne(){
-	n = n+1;
-	clickTracker.innerHTML = n.toString();
+function incrementCatOne(){
+	num1 = num1+1;
+	catOne.innerHTML = num1.toString();
 }
 
+function incrementCatTwo(){
+	num2 = num2+1;
+	catTwo.innerHTML = num2.toString();
+}
